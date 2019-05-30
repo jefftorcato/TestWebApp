187b60edbcaa341e1b01036a29263106186d0da5
jefftorcato <jefftorcato@gmail.com>
2019-05-30 21:22:26 +0530

    Minor

--- dist/js/client.js
+++ dist/js/client.js
@@ -6,16 +6,16 @@ MyClient.prototype.init = function () {
     var address = 'http://192.168.1.35:2017';
     console.log("Connecting to: " + address);
     this.socket = io.connect(address);
-    this.socket.on('connect', this.proxy(function(){
+    this.socket.on('connect', function(){
         console.log("Connected to socket!");
         this.socketIsConnected = true;
 
-        this.socket.on('disconnect', this.proxy(function(){
+        this.socket.on('disconnect', function(){
             console.log("Disconnected from socket!")
             this.socketIsConnected = false;
-        }));
-        this.socket.on('musicBlob', this.proxy(this.onDataReceived))
-    }));
+        });
+        this.socket.on('data', this.onDataReceived)
+    });
 }
 
 MyClient.prototype.onDataReceived = function (data) {
